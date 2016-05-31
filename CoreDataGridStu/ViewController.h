7f4dc181118a5cba64284b83ff4536a8822b0232
ssn <ssnfee@gmail.com>
2016-05-31 10:51:27 +0800

    Initial Commit

---
+++ CoreDataGridStu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataGridStu
+//
+//  Created by sseen on 16/5/31.
+//  Copyright © 2016年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataGridStu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataGridStu
+//
+//  Created by sseen on 16/5/31.
+//  Copyright © 2016年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
