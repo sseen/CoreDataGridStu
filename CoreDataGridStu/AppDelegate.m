34bf796f42527e6e0da75d9c9bd7ea36fe21bc05
ssn <ssnfee@gmail.com>
2016-06-01 15:26:53 +0800

    nsmanagedobject 怎么才能不插入，只有一个插入的初始化方法

--- CoreDataGridStu/AppDelegate.m
+++ CoreDataGridStu/AppDelegate.m
@@ -18,7 +18,8 @@
 
 @interface AppDelegate ()
 @property (nonatomic, strong) CoreDataStack* coreDataStack;
-
+@property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, strong) NSFetchedResultsController *courseFetchResultsController;
 @end
 
 @implementation AppDelegate
@@ -41,6 +42,8 @@
     if (results == 0) {
         [self importJSONSeedData];
     }
+    
+    self.courseFetchResultsController;
 }
 
 - (void)importJSONSeedData {
@@ -61,15 +64,6 @@
         for (NSDictionary *innerDic in innerArray) {
             
             
-            NSArray *weekOfYearArray = innerDic[@"week_of_year"];
-            WeekOfYear *weekoy = [[WeekOfYear alloc] initWithEntity:weekOfYearEntity insertIntoManagedObjectContext:_coreDataStack.context];
-            NSMutableOrderedSet *mutableOrderSet = [NSMutableOrderedSet orderedSet];
-            for (NSString *item in weekOfYearArray) {
-                weekoy.weekOfYear = [NSNumber numberWithInteger:[item integerValue]];
-                [_coreDataStack saveContext];
-                [mutableOrderSet addObject:weekoy];
-            }
-            
             Course *course = [[Course alloc] initWithEntity:courseEntity insertIntoManagedObjectContext:_coreDataStack.context];
             course.name = name;
             NSString *time = innerDic[@"time"];
@@ -80,11 +74,107 @@
             course.teachers = innerDic[@"teachers"][0];
             course.weekday = [NSNumber numberWithInteger:[innerDic[@"weekday"] integerValue]];
             
+            NSArray *weekOfYearArray = innerDic[@"week_of_year"];
+            
+            NSMutableOrderedSet *mutableOrderSet = [NSMutableOrderedSet orderedSet];
+            for (NSString *item in weekOfYearArray) {
+                // 查找是不是已经有 weekofyear
+                NSUInteger index = [self.fetchedResultsController.fetchedObjects indexOfObjectPassingTest:^BOOL(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                    return ([[(WeekOfYear *)obj weekOfYear] integerValue] == item.integerValue);
+                }];
+                //
+                
+                WeekOfYear *weekoy = [[WeekOfYear alloc] initWithEntity:weekOfYearEntity insertIntoManagedObjectContext:_coreDataStack.context];
+                weekoy.weekOfYear = [NSNumber numberWithInteger:[item integerValue]];
+                if (index == NSNotFound) {
+                    NSError *error;
+                    [_coreDataStack saveContext];
+                    NSLog(@"week of year: %@", error);
+                    [_fetchedResultsController performFetch:&error];
+                    NSLog(@"week of year fetch : %@", error);
+                }else {
+                    // 这点有多坑，如果不处理，将所有的都添加了
+                    [_coreDataStack.context deleteObject:weekoy];
+                }
+                
+                [mutableOrderSet addObject:weekoy];
+            }
+            
+            //
             course.week_of_year = mutableOrderSet;
             [_coreDataStack saveContext];
+            
+            
+            [_fetchedResultsController performFetch:&error];
+            NSLog(@"week of year fetch : %@", error);
         }
         
     }
+}
+
+- (NSFetchedResultsController *)fetchedResultsController
+{
+    if (_fetchedResultsController != nil) {
+        return _fetchedResultsController;
+    }
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    
+    NSEntityDescription *entity = [NSEntityDescription
+                                   entityForName:@"WeekOfYear"
+                                   inManagedObjectContext:_coreDataStack.context];
+    [fetchRequest setEntity:entity];
+    NSSortDescriptor *sort = [[NSSortDescriptor alloc]
+                              initWithKey:@"weekOfYear" ascending:NO];
+    [fetchRequest setSortDescriptors:[NSArray arrayWithObject:sort]];
+    
+    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc]  initWithFetchRequest:fetchRequest
+                                                                                                 managedObjectContext:_coreDataStack.context
+                                                                                                   sectionNameKeyPath:nil
+                                                                                                            cacheName:nil];
+    
+    _fetchedResultsController = aFetchedResultsController;
+    
+    NSError *error = nil;
+    if (![_fetchedResultsController performFetch:&error]) {
+        NSLog(@"Core data error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    return _fetchedResultsController;
+}
+
+- (NSFetchedResultsController *)courseFetchResultsController
+{
+    if (_courseFetchResultsController != nil) {
+        return _courseFetchResultsController;
+    }
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    
+    NSEntityDescription *entity = [NSEntityDescription
+                                   entityForName:@"Course"
+                                   inManagedObjectContext:_coreDataStack.context];
+    NSSortDescriptor *sort = [[NSSortDescriptor alloc]
+                              initWithKey:@"name" ascending:NO];
+    NSPredicate    *predicate    = [NSPredicate predicateWithFormat:@"name == [cd] %@", @"高等数学A（二）"];
+
+    [fetchRequest setEntity:entity];
+    [fetchRequest setSortDescriptors:[NSArray arrayWithObject:sort]];
+    [fetchRequest setPredicate:predicate];
+    
+    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc]  initWithFetchRequest:fetchRequest
+                                                                                                 managedObjectContext:_coreDataStack.context
+                                                                                                   sectionNameKeyPath:nil
+                                                                                                            cacheName:nil];
+    
+    _courseFetchResultsController = aFetchedResultsController;
+    
+    NSError *error = nil;
+    if (![_courseFetchResultsController performFetch:&error]) {
+        NSLog(@"Core data error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    return _courseFetchResultsController;
 }
 
 @end
